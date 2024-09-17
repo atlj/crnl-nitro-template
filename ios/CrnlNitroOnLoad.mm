745910314bb6a8ca6ff9422ab3c9372ab42b3fe1
Burak Güner <burak.guner@callstack.com>
2024-09-17 18:48:15 +0300

    chore: create necessary ios code

---
+++ ios/CrnlNitroOnLoad.mm
@@ -0,0 +1,16 @@
+#import <Foundation/Foundation.h>
+#import <NitroModules/HybridObjectRegistry.hpp>
+
+@interface <<iosModulename>>OnLoad : NSObject
+@end
+
+@implementation <<iosModulename>>OnLoad
+
+using namespace margelo::nitro;
+
++ (void)load {
+  // TODO: Register all Swift Hybrid Objects here
+  // TODO: Register all C++ Hybrid Objects here
+}
+
+@end
